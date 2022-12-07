5c2d8de068e478729cf7ea309aaa14b1b6d7b5ee
lsc <516065524@qq.com>
2022-12-07 18:03:28 +0800

    first commit

---
+++ dist/cli.js
@@ -0,0 +1,147 @@
+"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; } function _optionalChain(ops) { let lastAccessLHS = undefined; let value = ops[0]; let i = 1; while (i < ops.length) { const op = ops[i]; const fn = ops[i + 1]; i += 2; if ((op === 'optionalAccess' || op === 'optionalCall') && value == null) { return undefined; } if (op === 'access' || op === 'optionalAccess') { lastAccessLHS = value; value = fn(value); } else if (op === 'call' || op === 'optionalCall') { value = fn((...args) => value.call(lastAccessLHS, ...args)); lastAccessLHS = undefined; } } return value; }// src/node/cli.ts
+var _cac = require('cac'); var _cac2 = _interopRequireDefault(_cac);
+var _path = require('path');
+
+// src/node/build.ts
+var _vite = require('vite');
+
+// src/node/constants/index.ts
+
+var PACKAGE_ROOT = _path.join.call(void 0, __dirname, "..");
+var RUNTIME_PATH = _path.join.call(void 0, PACKAGE_ROOT, "src", "runtime");
+var CLIENT_ENTRY_PATH = _path.join.call(void 0, RUNTIME_PATH, "client-entry.tsx");
+var SERVER_ENTRY_PATH = _path.join.call(void 0, RUNTIME_PATH, "ssr-entry.tsx");
+var DEFAULT_HTML_PATH = _path.join.call(void 0, PACKAGE_ROOT, "template.html");
+
+// src/node/build.ts
+
+var _fsextra = require('fs-extra'); var _fsextra2 = _interopRequireDefault(_fsextra);
+var _ora = require('ora'); var _ora2 = _interopRequireDefault(_ora);
+async function bundle(root) {
+  const resolveViteConfig = (isServer) => ({
+    mode: "production",
+    root,
+    build: {
+      ssr: isServer,
+      outDir: isServer ? ".temp" : "build",
+      rollupOptions: {
+        input: isServer ? SERVER_ENTRY_PATH : CLIENT_ENTRY_PATH,
+        output: {
+          format: isServer ? "cjs" : "esm"
+        }
+      }
+    }
+  });
+  const spinner = _ora2.default.call(void 0, );
+  try {
+    const [clientBundle, serverBundle] = await Promise.all([
+      _vite.build.call(void 0, resolveViteConfig(false)),
+      _vite.build.call(void 0, resolveViteConfig(true))
+    ]);
+    return [clientBundle, serverBundle];
+  } catch (e) {
+    console.log(e);
+  }
+}
+async function renderPage(render, root, clientBundle) {
+  const clientChunk = clientBundle.output.find(
+    (chunk) => chunk.type === "chunk" && chunk.isEntry
+  );
+  console.log(`Rendering page in server side...`);
+  const appHtml = render();
+  const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>title</title>
+    <meta name="description" content="xxx">
+  </head>
+  <body>
+    <div id="root">${appHtml}</div>
+    <script type="module" src="/${_optionalChain([clientChunk, 'optionalAccess', _ => _.fileName])}"><\/script>
+  </body>
+</html>`.trim();
+  await _fsextra2.default.ensureDir(_path.join.call(void 0, root, "build"));
+  await _fsextra2.default.writeFile(_path.join.call(void 0, root, "build/index.html"), html);
+  await _fsextra2.default.remove(_path.join.call(void 0, root, ".temp"));
+}
+async function build(root = process.cwd()) {
+  const [clientBundle] = await bundle(root);
+  const serverEntryPath = _path.join.call(void 0, root, ".temp", "ssr-entry.js");
+  const { render } = await Promise.resolve().then(() => require(serverEntryPath));
+  await renderPage(render, root, clientBundle);
+}
+
+// src/node/dev.ts
+
+
+// src/node/plugin-island/indexHtml.ts
+
+function pluginIndexHtml() {
+  return {
+    name: "island:index-html",
+    apply: "serve",
+    transformIndexHtml(html) {
+      return {
+        html,
+        tags: [
+          {
+            tag: "script",
+            attrs: {
+              type: "module",
+              src: `/@fs/${CLIENT_ENTRY_PATH}`
+            },
+            injectTo: "body"
+          }
+        ]
+      };
+    },
+    configureServer(server) {
+      return () => {
+        server.middlewares.use(async (req, res, next) => {
+          let html = await _fsextra2.default.readFile(DEFAULT_HTML_PATH, "utf-8");
+          try {
+            html = await server.transformIndexHtml(
+              req.url,
+              html,
+              req.originalUrl
+            );
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "text/html");
+            res.end(html);
+          } catch (e) {
+            return next(e);
+          }
+        });
+      };
+    }
+  };
+}
+
+// src/node/dev.ts
+var _pluginreact = require('@vitejs/plugin-react'); var _pluginreact2 = _interopRequireDefault(_pluginreact);
+function createDevServer(root) {
+  return _vite.createServer.call(void 0, {
+    root,
+    plugins: [pluginIndexHtml(), _pluginreact2.default.call(void 0, )]
+  });
+}
+
+// src/node/cli.ts
+var cli = _cac2.default.call(void 0, "island").version("0.0.1").help();
+cli.command("dev [root]", "start dev server").action(async (root) => {
+  const server = await createDevServer(root);
+  await server.listen();
+  server.printUrls();
+});
+cli.command("build [root]", "build in production").action(async (root) => {
+  try {
+    root = _path.resolve.call(void 0, root);
+    await build(root);
+  } catch (e) {
+    console.log(e);
+  }
+});
+cli.parse();
---
+++ dist/cli.mjs
@@ -0,0 +1,154 @@
+// node_modules/.pnpm/registry.npmmirror.com+tsup@6.5.0_typescript@4.8.4/node_modules/tsup/assets/esm_shims.js
+import { fileURLToPath } from "url";
+import path from "path";
+var getFilename = () => fileURLToPath(import.meta.url);
+var getDirname = () => path.dirname(getFilename());
+var __dirname = /* @__PURE__ */ getDirname();
+
+// src/node/cli.ts
+import cac from "cac";
+import { resolve } from "path";
+
+// src/node/build.ts
+import { build as viteBuild } from "vite";
+
+// src/node/constants/index.ts
+import { join } from "path";
+var PACKAGE_ROOT = join(__dirname, "..");
+var RUNTIME_PATH = join(PACKAGE_ROOT, "src", "runtime");
+var CLIENT_ENTRY_PATH = join(RUNTIME_PATH, "client-entry.tsx");
+var SERVER_ENTRY_PATH = join(RUNTIME_PATH, "ssr-entry.tsx");
+var DEFAULT_HTML_PATH = join(PACKAGE_ROOT, "template.html");
+
+// src/node/build.ts
+import { join as join2 } from "path";
+import fs from "fs-extra";
+import ora from "ora";
+async function bundle(root) {
+  const resolveViteConfig = (isServer) => ({
+    mode: "production",
+    root,
+    build: {
+      ssr: isServer,
+      outDir: isServer ? ".temp" : "build",
+      rollupOptions: {
+        input: isServer ? SERVER_ENTRY_PATH : CLIENT_ENTRY_PATH,
+        output: {
+          format: isServer ? "cjs" : "esm"
+        }
+      }
+    }
+  });
+  const spinner = ora();
+  try {
+    const [clientBundle, serverBundle] = await Promise.all([
+      viteBuild(resolveViteConfig(false)),
+      viteBuild(resolveViteConfig(true))
+    ]);
+    return [clientBundle, serverBundle];
+  } catch (e) {
+    console.log(e);
+  }
+}
+async function renderPage(render, root, clientBundle) {
+  const clientChunk = clientBundle.output.find(
+    (chunk) => chunk.type === "chunk" && chunk.isEntry
+  );
+  console.log(`Rendering page in server side...`);
+  const appHtml = render();
+  const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>title</title>
+    <meta name="description" content="xxx">
+  </head>
+  <body>
+    <div id="root">${appHtml}</div>
+    <script type="module" src="/${clientChunk?.fileName}"><\/script>
+  </body>
+</html>`.trim();
+  await fs.ensureDir(join2(root, "build"));
+  await fs.writeFile(join2(root, "build/index.html"), html);
+  await fs.remove(join2(root, ".temp"));
+}
+async function build(root = process.cwd()) {
+  const [clientBundle] = await bundle(root);
+  const serverEntryPath = join2(root, ".temp", "ssr-entry.js");
+  const { render } = await import(serverEntryPath);
+  await renderPage(render, root, clientBundle);
+}
+
+// src/node/dev.ts
+import { createServer } from "vite";
+
+// src/node/plugin-island/indexHtml.ts
+import fs2 from "fs-extra";
+function pluginIndexHtml() {
+  return {
+    name: "island:index-html",
+    apply: "serve",
+    transformIndexHtml(html) {
+      return {
+        html,
+        tags: [
+          {
+            tag: "script",
+            attrs: {
+              type: "module",
+              src: `/@fs/${CLIENT_ENTRY_PATH}`
+            },
+            injectTo: "body"
+          }
+        ]
+      };
+    },
+    configureServer(server) {
+      return () => {
+        server.middlewares.use(async (req, res, next) => {
+          let html = await fs2.readFile(DEFAULT_HTML_PATH, "utf-8");
+          try {
+            html = await server.transformIndexHtml(
+              req.url,
+              html,
+              req.originalUrl
+            );
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "text/html");
+            res.end(html);
+          } catch (e) {
+            return next(e);
+          }
+        });
+      };
+    }
+  };
+}
+
+// src/node/dev.ts
+import pluginReact from "@vitejs/plugin-react";
+function createDevServer(root) {
+  return createServer({
+    root,
+    plugins: [pluginIndexHtml(), pluginReact()]
+  });
+}
+
+// src/node/cli.ts
+var cli = cac("island").version("0.0.1").help();
+cli.command("dev [root]", "start dev server").action(async (root) => {
+  const server = await createDevServer(root);
+  await server.listen();
+  server.printUrls();
+});
+cli.command("build [root]", "build in production").action(async (root) => {
+  try {
+    root = resolve(root);
+    await build(root);
+  } catch (e) {
+    console.log(e);
+  }
+});
+cli.parse();
